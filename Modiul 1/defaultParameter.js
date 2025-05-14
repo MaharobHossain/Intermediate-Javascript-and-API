b753bfdf1c0eba2069148bee3ccd27de74ae3aa4
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-05-14 16:59:01 +0600

    Template string, multiple line string, dynamic string

--- Modiul 1/defaultParameter.js
+++ Modiul 1/defaultParameter.js
@@ -1,12 +1,17 @@
-
 const email = 'hi rakib \n' +
 'i am maharob';
-
 console.log(email);
-
 
 
 const mail = `hi maharob
 i am sumiya from microsoft head quater office.
 how can i help u sir?`;
 console.log(mail);
+
+
+const a = 10;
+const b = 20;
+const sum = 'The sum of num ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(sum);
+const calculation = `The sum of num ${a} and ${b} is ${a + b}`;
+console.log(calculation);
