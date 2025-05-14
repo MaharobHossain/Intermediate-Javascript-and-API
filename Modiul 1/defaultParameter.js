01d7faade5a64a619d21c44903afaa59056d01f4
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-05-14 16:48:36 +0600

    Template string, multiple line string, dynamic string

--- Modiul 1/defaultParameter.js
+++ Modiul 1/defaultParameter.js
@@ -1,2 +1,12 @@
 
-console.log("added file");
+const email = 'hi rakib \n' +
+'i am maharob';
+
+console.log(email);
+
+
+
+const mail = `hi maharob
+i am sumiya from microsoft head quater office.
+how can i help u sir?`;
+console.log(mail);
