cb690f54eec985944ab3765caac164a5bbe617b9
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-05-27 06:28:10 +0600

    More Arrow functions and big arrow function

--- Modiul 1/arrayFunction.js
+++ Modiul 1/arrayFunction.js
@@ -6,3 +6,8 @@ const getAge =(person) => person.age;
 const student = {name: 'ananta', age: 45}
 const age = getAge(student);
 console.log(age); 
+
+
+const getThird = numbers => numbers[2];
+const third = getThird([5,6,74,8,9,34,65]);
+console.log(third);
