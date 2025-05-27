1439600096ee0ba85a76e4be2f882c757a093afc
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-05-27 06:32:32 +0600

    More Arrow functions and big arrow function

--- Modiul 1/arrayFunction.js
+++ Modiul 1/arrayFunction.js
@@ -2,6 +2,7 @@
 const difference = ( x, y ) => x - y;
 const multiply = (frist, second, third) => frist * second * third;
 
+// singel parameter or one parameter
 const getAge =(person) => person.age;
 const student = {name: 'ananta', age: 45}
 const age = getAge(student);
@@ -11,3 +12,7 @@ console.log(age);
 const getThird = numbers => numbers[2];
 const third = getThird([5,6,74,8,9,34,65]);
 console.log(third);
+
+// no parameter
+const getPI = () => Math.PI
+console.log(getPI());
