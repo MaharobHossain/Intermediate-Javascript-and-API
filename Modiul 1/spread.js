322131372e574be54eca15a45806678f94461c0d
MD. RAKIB HOSSEN SARKAR <sarkar12ab@gmail.com>
2025-06-03 15:25:06 +0600

    Normal max numbers and array max numbers find

---
+++ Modiul 1/spread.js
@@ -0,0 +1,10 @@
+// Normal numbers ar moddhe theke max number ber kora
+const maxNumber = Math.max(12, 45, 23, 75);
+console.log(maxNumber);
+
+
+// Array theke max number ber kora
+
+const numbers = [ 32, 45, 67, 97, 27];
+const arrayMax = Math.max(...numbers);
+console.log(arrayMax);
