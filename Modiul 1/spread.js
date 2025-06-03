23016c3f422904632a3095d0d2afc21b82e26dd1
MD. RAKIB HOSSEN SARKAR <sarkar12ab@gmail.com>
2025-06-03 15:57:08 +0600

    Use spreed operato

--- Modiul 1/spread.js
+++ Modiul 1/spread.js
@@ -8,3 +8,14 @@ console.log(maxNumber);
 const numbers = [ 32, 45, 67, 97, 27];
 const arrayMax = Math.max(...numbers);
 console.log(arrayMax);
+
+
+
+//Use spreed operator to copy
+const nums = [2, 4 ,5, 7, 34, 86];
+const num2 = nums;
+const num3 = [...num2];
+num3.push(100);
+console.log(nums);
+console.log(num2);
+console.log(num3);
