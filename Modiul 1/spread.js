a8c6a5b2f7b16539039bba6c232778a3572bf700
MaharobHossain <maharobhossain8880@gmail.com>
2025-06-03 16:35:36 +0600

    testing

--- Modiul 1/spread.js
+++ Modiul 1/spread.js
@@ -11,11 +11,3 @@ console.log(arrayMax);
 
 
 
-//Use spreed operator to copy
-const nums = [2, 4 ,5, 7, 34, 86];
-const num2 = nums;
-const num3 = [...num2];
-num3.push(100);
-console.log(nums);
-console.log(num2);
-console.log(num3);
